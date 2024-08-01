5b241178615c01e2606af30b1a9aaf491f761014
Thibault Monnier <thibaultmonni@gmail.com>
2024-08-01 21:21:46 +0200

    Add remark-gfm dependency + a markdown test in Blog

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,8 +1,32 @@
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+
 export default function Blog() {
+    const markdownSentences1 = [
+        '# Hi!',
+        'This is the **first** markdown file *ever* uploaded as a test!',
+        'Hey, watch our [Github](https://github.com/Eagletech-robotic/website)!',
+        '`This is inline code!`',
+        'And \n```\nthis is a block of code.\n```',
+        '1. And\n   - a\n   - nested\n    - list.\n2. a\n3. list.',
+    ]
+    const markdown1 = markdownSentences1.join('\n')
+
+    const markdownSentences2 = [
+        'Wow, look at that! remarkGfm plugin allows you to write links directly!',
+        'www.google.com',
+    ]
+    const markdown2 = markdownSentences2.join('\n')
+
     return (
         <>
             <h1>Blog Page</h1>
-            <div>Note: This is just a placeholder</div>
+            <h2>Our blogs:</h2>
+            <br></br>
+            <div style={{ border: '2px solid black' }}>
+                <Markdown>{markdown1}</Markdown>
+                <Markdown remarkPlugins={[remarkGfm]}>{markdown2}</Markdown>
+            </div>
         </>
     )
 }
