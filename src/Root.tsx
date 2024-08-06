db63ef97a9748aecaf3a4be07797a78aab6b6d67
Thibault Monnier <thibaultmonni@gmail.com>
2024-08-06 15:09:30 +0200

    Create a styled header

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,11 +1,19 @@
 import { NavLink, Outlet } from 'react-router-dom'
+import './root.css'
 
 export default function Root() {
     return (
         <>
-            <div style={{ backgroundColor: 'yellow' }}>
-                <NavLink to="about">About </NavLink>
-                <NavLink to="blog">Blog</NavLink>
+            <div className="header">
+                <NavLink to="about" className="button">
+                    <span>About</span>
+                </NavLink>
+                <NavLink to="blog" className="button">
+                    <span>Blog</span>
+                </NavLink>
+                <a href="https://github.com/Eagletech-robotic" className="button external">
+                    <span>Github</span>
+                </a>
             </div>
             <div style={{ backgroundColor: 'lightblue', overflow: 'hidden' }}>
                 <Outlet />
