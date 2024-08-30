4c132ccbf8b1db71e8ce0da217bfa9d121f22cd1
Thibault Monnier <thibaultmonni@gmail.com>
2024-08-30 17:57:02 +0200

    Add '-er' to date for the first day of the month

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,11 @@
 export function isoToFullDate(isoDate: string): string {
     const dateObj = new Date(isoDate)
     const options: Intl.DateTimeFormatOptions = { day: 'numeric', month: 'long', year: 'numeric' }
-    return dateObj.toLocaleDateString('fr-FR', options)
+
+    let dateString = dateObj.toLocaleDateString('fr-FR', options)
+    if (dateObj.getDate() === 1) {
+        dateString = dateString.replace('1', '1er')
+    }
+
+    return dateString
 }
