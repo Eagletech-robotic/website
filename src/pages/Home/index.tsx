36db8ac7a82a36b83b807227d4eb97fd207bd0af
Thibault Monnier <thibaultmonni@gmail.com>
2024-10-04 21:38:10 +0200

    Largely improve Home Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,35 @@
-import { Header, Logo, Page, Subtitle, Title } from './styles'
+import { Link } from 'react-router-dom'
+import {
+    Button,
+    Header,
+    HeaderContainer,
+    Logo,
+    Page,
+    Section1,
+    Section2,
+    SectionTitle,
+    Subtitle,
+    Title,
+} from './styles'
 
 export default function Home() {
     return (
         <Page>
-            <Header>
-                <Logo />
-                <Title>EagleTech</Title>
-                <Subtitle>Robotics</Subtitle>
-            </Header>
+            <HeaderContainer>
+                <Header>
+                    <Logo />
+                    <Title>EagleTech</Title>
+                    <Subtitle>Robotics</Subtitle>
+                </Header>
+            </HeaderContainer>
+
+            <Section1>
+                <SectionTitle>Nous sommes une équipe française</SectionTitle>
+                <Link to="/about">
+                    <Button>Découvrir Plus!</Button>
+                </Link>
+            </Section1>
+            <Section2>Second Section</Section2>
         </Page>
     )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,11 @@
-import { breakpoints, colors, fontSizes, headerHeight, mixin } from 'src/styles/commonStyles'
+import {
+    breakpoints,
+    colors,
+    fontSizes,
+    headerHeight,
+    hoverTransitionTimingFunction,
+    mixin,
+} from 'src/styles/commonStyles'
 import styled from 'styled-components'
 
 export const Page = styled.div`
@@ -9,27 +16,42 @@ export const Page = styled.div`
     color: ${colors.textWhite};
 `
 
-export const Header = styled.div`
+export const HeaderContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
     height: calc(100vh - ${headerHeight.mobile});
     position: relative;
     z-index: 1;
 
     @media (min-width: ${breakpoints.desktop}) {
-        height: calc(100vh - ${headerHeight.desktop});
+        height: 150vh;
+    }
+`
+
+export const Header = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 10rem;
+    z-index: 1;
+
+    position: sticky;
+    top: 9rem;
+
+    @media (min-width: ${breakpoints.tablet}) {
+        margin-top: 20rem;
     }
 `
 
 export const Title = styled.h1`
-    font-size: ${fontSizes.mobile.title};
+    font-size: 4rem;
     font-weight: bold;
     margin-bottom: 1rem;
 
     @media (min-width: ${breakpoints.tablet}) {
-        font-size: ${fontSizes.desktop.title};
+        font-size: 7rem;
     }
 `
 
@@ -53,5 +75,66 @@ export const Logo = styled.div`
     @media (min-width: ${breakpoints.tablet}) {
         height: 35rem;
         width: 35rem;
+    }
+`
+
+const Section = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 15rem 0;
+`
+
+export const Section1 = styled(Section)`
+    background-color: ${colors.darkBackground};
+`
+
+export const Section2 = styled(Section)`
+    background-color: ${colors.darkBackground};
+`
+
+export const Button = styled.button`
+    margin-top: 2rem;
+    border: 1px solid ${colors.primary};
+    background-color: transparent;
+    padding: 1rem 4rem 1rem 3rem;
+    color: ${colors.textWhite};
+    border-radius: 2rem;
+
+    transition: background-color 0.5s ${hoverTransitionTimingFunction},
+        color 0.5s ${hoverTransitionTimingFunction};
+
+    &::after {
+        content: '';
+        position: absolute;
+        display: inline-block;
+        width: 1em;
+        height: 1em;
+        margin-left: 1rem;
+        vertical-align: middle;
+        background-color: ${colors.textWhite};
+        mask: url('/images/button-arrow.svg') no-repeat center;
+        mask-size: contain;
+        transition: margin-left 0.5s ${hoverTransitionTimingFunction};
+    }
+
+    &:hover {
+        background-color: ${colors.primary};
+        color: ${colors.text};
+
+        &::after {
+            background-color: ${colors.text};
+            margin-left: 1.5rem;
+        }
+    }
+`
+
+export const SectionTitle = styled.h2`
+    font-size: ${fontSizes.mobile.heading2};
+    text-align: center;
+
+    @media (min-width: ${breakpoints.tablet}) {
+        font-size: ${fontSizes.desktop.heading2};
     }
 `
