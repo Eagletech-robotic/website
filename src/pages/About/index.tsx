77ffd34e8ac7a70164633fc2c6dbb049a1e929b8
Antoine Monnier <monniera@gmail.com>
2024-11-29 13:46:32 +0100

    About: change member order

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -43,30 +43,6 @@ export default function About() {
 
 const MemberPresentations = [
     {
-        photo: photoAntoine,
-        title: 'Antoine Monnier - Rédaction technique, Développement, Mécanique',
-        description: `Je suis ingénieur de formation et entrepreneur en informatique.
-        Passionné de robotique depuis toujours, je n’avais jamais eu l’occasion de faire partie d’une équipe.
-        Après avoir accompagné Thibault à la compétition de robotique FIRST en 2024,
-        mes amis Nicolas et Marion m’ont parlé de la Coupe de France de Robotique à laquelle ils venaient d’assister.
-        Ils nous ont donné l’envie de monter une équipe avec nos enfants, tous très enthousiastes à cette idée.
-        Je vais contribuer aux articles techniques du blog et participer à l’écriture des algorithmes de pilotage
-        et de vision. Je m’impliquerai également dans la construction de prototypes,
-        comme la pelle du robot et d’autres accessoires. J’ai surtout à cœur de partager notre expérience
-        et nos apprentissages, avec nos enfants mais aussi avec le public que nous allons rencontrer
-        dans les événements scientifiques auxquels nous allons participer.
-        `,
-    },
-    {
-        photo: photoJanaina,
-        title: 'Janaina Wittner - Logistique',
-        description: `Responsable des partenariats dans ma vie professionnelle et mère de Louis et de Thibault,
-        je m’efforce avant tout de motiver les équipes et de les accompagner dans la logistique : 
-        organisation des rencontres nécessaires à la réalisation des projets, mise à disposition d'espaces de travail,
-        transport des personnes et des robots, ainsi que l'hébergement sur place.
-        `,
-    },
-    {
         photo: photoLouis,
         title: 'Louis Monnier – Communication et rédaction',
         description: `Je suis étudiant en première au CIV (Centre International de Valbonne) dans les Alpes-Maritimes.
@@ -80,29 +56,6 @@ const MemberPresentations = [
         Mes activités iront de l’écriture de contenu pour notre site,
         notamment des articles pour le blog à la présentation de notre projet au CIV et des associations locales
         ainsi que la décoration et l’animation du stand le jour de la compétition. 
-        `,
-    },
-    {
-        photo: photoMarion,
-        title: 'Marion Boulay – Mécanique-Électronique',
-        description: `Je m’appelle Marion, j’ai participé en 2003 et 2004 à la coupe de France de Robotique
-        avec mon école (Projet&Tech). En 2004 j’ai rencontré mon mari à la coupe.
-        Nous avons décidé de participer en indépendant en 2005 et 2007 sous l’équipe Astromech.
-        L’année dernière pour fêter nos 20 ans de rencontre, nous sommes montés à la coupe
-        afin de la présenter à nos enfants. Ils ont beaucoup aimé et ont souhaité participer,
-        c’est la raison pour laquelle nous en avons parlé à des amis et choisi de retenter l’aventure cette année.
-        Dans le cadre de ce projet de robotique, je vais réaliser la mécanique
-        et l’électronique du robot principal. Je m’engage à partager mon expérience,
-        notre défi côté mécanique : réaliser la mécanique avec un seul moteur/servo-moteur.
-        J’ai aussi le projet de partager ma passion pour que mes enfants et tout autre personne
-        de leur âge soient passionnés par ce domaine et qu’ils puissent voir et comprendre
-        qu’à tout âge on peut faire de la robotique, et apprendre en s’amusant.
-        `,
-    },
-    {
-        photo: photoNicolas,
-        title: 'Nicolas Boulay - Informatique-Electronique',
-        description: `J’ai participé à la coupe en 1999 dont le règlement était sur les châteaux forts. Les années suivantes, j’ai été arbitre puis chef arbitre Eurobot (l’année des Palets). En 2005 et 2007, j’ai refait une équipe avec Marion et des amis. L’an passé, la conjonction des jours fériés à permis de faire le trajet depuis Nice pour montrer la coupe à nos adolescents. Cela m’a donné envie de recommencer surtout avec l’évolution des technologies disponibles. Je vais réaliser surtout l’électronique et l’informatique bas niveau du robot.
         `,
     },
     {
@@ -136,6 +89,53 @@ const MemberPresentations = [
     {
         photo: photoThibault,
         title: 'Thibault Monnier - Développeur et mécanicien',
-        description: '...',
+        description: `Je suis étudiant en troisième au Centre International de Valbonne (CIV) dans les Alpes-Maritimes où, bien que plus jeune que les autres, j'ai rejoint le club de robotique du lycée sur l’année 2023-2024. Je suis passionné par la programmation avec plusieurs projets open sources disponibles sur mon compte Github. J’ai d’ailleurs récemment commencé l'algorithmique ; enfin, j’adore les maths. Je conçois, en complète autonomie, le site internet d’EagleTech et je travaille sur le développement d’un simulateur pour tester l’algorithme de pilotage du robot. Je compte toucher à tout le code de notre club.`,
+    },
+    {
+        photo: photoAntoine,
+        title: 'Antoine Monnier - Rédaction technique, Développement, Mécanique',
+        description: `Je suis ingénieur de formation et entrepreneur en informatique.
+        Passionné de robotique depuis toujours, je n’avais jamais eu l’occasion de faire partie d’une équipe.
+        Après avoir accompagné Thibault à la compétition de robotique FIRST en 2024,
+        mes amis Nicolas et Marion m’ont parlé de la Coupe de France de Robotique à laquelle ils venaient d’assister.
+        Ils nous ont donné l’envie de monter une équipe avec nos enfants, tous très enthousiastes à cette idée.
+        Je vais contribuer aux articles techniques du blog et participer à l’écriture des algorithmes de pilotage
+        et de vision. Je m’impliquerai également dans la construction de prototypes,
+        comme la pelle du robot et d’autres accessoires. J’ai surtout à cœur de partager notre expérience
+        et nos apprentissages, avec nos enfants mais aussi avec le public que nous allons rencontrer
+        dans les événements scientifiques auxquels nous allons participer.
+        `,
+    },
+    {
+        photo: photoJanaina,
+        title: 'Janaina Wittner - Logistique',
+        description: `Responsable des partenariats dans ma vie professionnelle et mère de Louis et de Thibault,
+        je m’efforce avant tout de motiver les équipes et de les accompagner dans la logistique : 
+        organisation des rencontres nécessaires à la réalisation des projets, mise à disposition d'espaces de travail,
+        transport des personnes et des robots, ainsi que l'hébergement sur place.
+        `,
+    },
+    {
+        photo: photoMarion,
+        title: 'Marion Boulay – Mécanique-Électronique',
+        description: `Je m’appelle Marion, j’ai participé en 2003 et 2004 à la coupe de France de Robotique
+        avec mon école (Projet&Tech). En 2004 j’ai rencontré mon mari à la coupe.
+        Nous avons décidé de participer en indépendant en 2005 et 2007 sous l’équipe Astromech.
+        L’année dernière pour fêter nos 20 ans de rencontre, nous sommes montés à la coupe
+        afin de la présenter à nos enfants. Ils ont beaucoup aimé et ont souhaité participer,
+        c’est la raison pour laquelle nous en avons parlé à des amis et choisi de retenter l’aventure cette année.
+        Dans le cadre de ce projet de robotique, je vais réaliser la mécanique
+        et l’électronique du robot principal. Je m’engage à partager mon expérience,
+        notre défi côté mécanique : réaliser la mécanique avec un seul moteur/servo-moteur.
+        J’ai aussi le projet de partager ma passion pour que mes enfants et tout autre personne
+        de leur âge soient passionnés par ce domaine et qu’ils puissent voir et comprendre
+        qu’à tout âge on peut faire de la robotique, et apprendre en s’amusant.
+        `,
+    },
+    {
+        photo: photoNicolas,
+        title: 'Nicolas Boulay - Informatique-Electronique',
+        description: `J’ai participé à la coupe en 1999 dont le règlement était sur les châteaux forts. Les années suivantes, j’ai été arbitre puis chef arbitre Eurobot (l’année des Palets). En 2005 et 2007, j’ai refait une équipe avec Marion et des amis. L’an passé, la conjonction des jours fériés à permis de faire le trajet depuis Nice pour montrer la coupe à nos adolescents. Cela m’a donné envie de recommencer surtout avec l’évolution des technologies disponibles. Je vais réaliser surtout l’électronique et l’informatique bas niveau du robot.
+        `,
     },
 ]
